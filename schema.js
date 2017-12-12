d85b7e30e91f1a3f521167f84407e0d06868d744
Anthony Giniers <antogyn@gmail.com>
2017-12-12 19:52:30 +0100

    Add schema

--- schema.js
+++ schema.js
@@ -1,7 +1,69 @@
 const { makeExecutableSchema } = require('graphql-tools');
 const gql = require('./gql');
 
+const typeDef = gql`
+type Query {
+  human(id: ID!): Human
+}
+
+type Human {
+  id: ID!
+  name: String!
+  friends: [Human]
+}
+`;
+
+const humans = {
+  '1': {
+    id: '1',
+    name: 'Anakin Skywalker',
+    friendIds: ['2']
+  },
+  '2': {
+    id: '2',
+    name: 'Obi-Wan Kenobi',
+    friendIds: ['1', '3']
+  },
+  '3': {
+    id: '3',
+    name: 'Luke Skywalker',
+    friendIds: ['2']
+  },
+  '4': {
+    id: '4',
+    name: 'Boba Fett',
+    friendIds: []
+  },
+};
+
+function getHumanById(id) {
+  return humans[id];
+}
+
+function getManyHumanByIds(ids) {
+  return ids.map(getHumanById);
+}
+
+const Resolvers = {
+  Query: {
+    human(_, { id }) {
+      return getHumanById(id);
+    },
+  },
+  Human: {
+    id(human) {
+      return human.id;
+    },
+    name(human) {
+      return human.name;
+    },
+    friends(human) {
+      return getManyHumanByIds(human.friendIds);
+    },
+  },
+};
+
 exports.graphqlSchema = makeExecutableSchema({
-  typeDefs: [ ?? ],
-  resolvers: ??,
+  typeDefs: [ typeDef ],
+  resolvers: Resolvers,
 });
